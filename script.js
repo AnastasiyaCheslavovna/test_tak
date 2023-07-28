49952cd2c66d44f6b023a5d313a27b11dcd0a71f
AnastasiyaCheslavovna <kashlejnastya@mail.ru>
2023-07-28 22:29:10 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,32 +1,24 @@
-let data = [
-  { name: "foo1", value: "bar1" },
-  { name: "foo2", value: "bar2" },
-  { name: "foo3", value: "bar3" },
-  { name: "foo4", value: "bar4" },
-  { name: "foo5", value: "bar5" },
-  { name: "foo6", value: "bar6" },
-  { name: "foo7", value: "bar7" },
-  { name: "foo8", value: "bar8" },
-  { name: "foo9", value: "bar9" },
-  { name: "foo10", value: "bar10" },
-  { name: "foo11", value: "bar11" },
-  { name: "foo12", value: "bar12" },
-  { name: "foo13", value: "bar13" },
-  { name: "foo14", value: "bar14" },
-  { name: "foo15", value: "bar15" },
-  { name: "foo16", value: "bar16" },
-  { name: "foo17", value: "bar17" },
-  { name: "foo18", value: "bar18" },
-  { name: "foo19", value: "bar19" },
-  { name: "foo20", value: "bar20" }
-];
-
+let data = [];
 let selectedItemId = null;
-let nameSortOrder = 0; // 0: no sort, 1: A-Z, -1: Z-A
-let valueSortOrder = 0; // 0: no sort, 1: A-Z, -1: Z-A
+let nameSortOrder = 0; 
+let valueSortOrder = 0; 
+let messageTimeoutId = null;
 
 window.onload = function() {
-  populateItemList();
+  data = getDataFromLocalStorage();
+
+  if (!data) {
+    fetch('data.json')
+      .then(response => response.json())
+      .then(jsonData => {
+        data = jsonData;
+        populateItemList();
+      })
+      .catch(error => console.error('Error:', error));
+  } else {
+    populateItemList();
+  }
+
   document.getElementById('filter').addEventListener('input', filterItems);
   document.getElementById('clearFilter').addEventListener('click', clearFilter);
   document.getElementById('sortByName').addEventListener('click', () => sortItems('name'));
@@ -37,9 +29,17 @@ window.onload = function() {
     event.preventDefault();
     document.getElementById('editForm').style.display = 'none'; 
   });
-  document.getElementById('listViewButton').addEventListener('click', switchToListView);
-  document.getElementById('gridViewButton').addEventListener('click', switchToGridView);
+  document.getElementById('listViewButton').addEventListener('click', () => switchView('list'));
+  document.getElementById('gridViewButton').addEventListener('click', () => switchView('grid'));
+}
 
+function getDataFromLocalStorage() {
+  const storedData = localStorage.getItem('myData');
+  return storedData ? JSON.parse(storedData) : null;
+}
+
+function saveDataToLocalStorage() {
+  localStorage.setItem('myData', JSON.stringify(data));
 }
 
 function createListItem(item, index) {
@@ -80,59 +80,77 @@ function populateItemList(dataToDisplay = data) {
 }
 
 function selectItem(event) {
-  if (event.target.classList.contains('list-group-item')) {
-    const clickedItemId = Number(event.target.value);
-    selectedItemId = clickedItemId;
-    document.getElementById('nameDisplay').value = data[selectedItemId].name;
-    document.getElementById('valueInput').value = data[selectedItemId].value;
-    document.getElementById('editForm').style.display = 'block'; 
-  }
+  const clickedItemId = Number(event.target.value);
+  selectedItemId = clickedItemId;
+  document.getElementById('nameDisplay').value = data[selectedItemId].name;
+  document.getElementById('valueInput').value = data[selectedItemId].value;
+  document.getElementById('editForm').style.display = 'block'; 
+  clearMessage();
 }
-
 
 function editItem(event) {
   event.preventDefault();
-  const messageElement = document.getElementById('errorMessage');
   if (selectedItemId === null) {
-    messageElement.textContent = 'Please select an item to edit.';
-    messageElement.className = 'alert alert-danger mt-3';
-    messageElement.style.display = 'block';
-    setTimeout(() => {
-      messageElement.style.display = 'none';
-    }, 5000);
+    displayMessage('Please select an item to edit.', 'danger');
     return;
   }
   const newValue = document.getElementById('valueInput').value;
+  updateItemValue(newValue);
+  displayMessage('Item updated successfully.', 'success');
+  saveDataToLocalStorage();
+}
+
+messageTimeoutId = setTimeout(() => {
+  messageElement.style.display = 'none';
+}, 5000);
+
+function clearMessage() {
+  const messageElement = document.getElementById('errorMessage');
+  messageElement.style.display = 'none';
+}
+
+function displayMessage(message, type) {
+  const messageElement = document.getElementById('errorMessage');
+  messageElement.textContent = message;
+  messageElement.className = `alert alert-${type} mt-3`;
+  messageElement.style.display = 'block';
+  if (messageTimeoutId) {
+    clearTimeout(messageTimeoutId);
+  }
+  messageTimeoutId = setTimeout(() => {
+    clearMessage();
+  }, 3000);
+}
+
+
+function updateItemValue(newValue) {
   data[selectedItemId].value = newValue;
   populateItemList();
-  messageElement.textContent = 'Item updated successfully.';
-  messageElement.className = 'alert alert-success mt-3';
-  messageElement.style.display = 'block';
-  setTimeout(() => {
-    messageElement.style.display = 'none';
-  }, 5000);
+}
+
+function updateSortOrderAndButton(sortOrder, buttonId, text) {
+  sortOrder = sortOrder === 0 ? 1 : -sortOrder;
+  document.getElementById(buttonId).textContent = `${text} ${sortOrder === 1 ? '▲' : '▼'}`;
+  return sortOrder;
 }
 
 function sortItems(type) {
   if (type === 'name') {
-    nameSortOrder = nameSortOrder === 0 ? 1 : -nameSortOrder;
-    data.sort((a, b) => nameSortOrder * a.name.localeCompare(b.name));
-    document.getElementById('sortByName').textContent = `Sort by Name ${nameSortOrder === 1 ? '▲' : '▼'}`;
+    nameSortOrder = updateSortOrderAndButton(nameSortOrder, 'sortByName', 'Sort by Name');
     valueSortOrder = 0;
     document.getElementById('sortByValue').textContent = 'Sort by Value';
   } else if (type === 'value') {
-    valueSortOrder = valueSortOrder === 0 ? 1 : -valueSortOrder;
-    data.sort((a, b) => valueSortOrder * a.value.localeCompare(b.value));
-    document.getElementById('sortByValue').textContent = `Sort by Value ${valueSortOrder === 1 ? '▲' : '▼'}`;
+    valueSortOrder = updateSortOrderAndButton(valueSortOrder, 'sortByValue', 'Sort by Value');
     nameSortOrder = 0;
     document.getElementById('sortByName').textContent = 'Sort by Name';
   }
+  data.sort((a, b) => (type === 'name' ? nameSortOrder : valueSortOrder) * a[type].localeCompare(b[type]));
   populateItemList();
 }
 
 function filterItems() {
   const filterValue = document.getElementById('filter').value.toLowerCase();
-  const filteredData = data.filter(item => item.name.toLowerCase().includes(filterValue) || item.value.toLowerCase().includes(filterValue));
+  const filteredData = data.filter(item => (item.name + item.value).toLowerCase().includes(filterValue));
   populateItemList(filteredData);
 }
 
@@ -144,18 +162,18 @@ function clearFilter() {
 function closeEditForm(event) {
   event.preventDefault();
   document.getElementById('editForm').style.display = 'none';
+  clearMessage(); 
 }
 
-function switchToListView() {
+function switchView(view) {
   const itemList = document.getElementById('itemList');
-  itemList.classList.remove('row');
-  itemList.classList.add('list-group'); 
+  if (view === 'list') {
+    itemList.classList.remove('row');
+    itemList.classList.add('list-group'); 
+  } else if (view === 'grid') {
+    itemList.classList.add('row'); 
+    itemList.classList.remove('list-group'); 
+  }
   populateItemList(); 
 }
 
-function switchToGridView() {
-  const itemList = document.getElementById('itemList');
-  itemList.classList.add('row'); 
-  itemList.classList.remove('list-group'); 
-  populateItemList(); 
-}
